8c96dd72fd156eb1704507b09dfadc4d3a69b3dc
Bruuno-Alves <bruno.a.santos@live.com>
2021-12-06 21:11:25 -0300

    Commit inicial

---
+++ clinica/src/tela/TelaPrincipal.java
@@ -0,0 +1,12 @@
+package tela;
+
+public class TelaPrincipal {
+
+	//Todo programa começa a ser executado a partir do método main
+	public static void main(String[] args) {
+
+		System.out.println("Programa para aprendizado da organização de pacotes e classes");
+
+	}
+
+}
