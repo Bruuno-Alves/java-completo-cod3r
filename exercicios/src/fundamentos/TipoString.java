871844e132e1583a43f55c8c2fd812869dca67b1
Bruuno-Alves <bruno.a.santos@live.com>
2021-12-11 12:55:10 -0300

    Tipo String

---
+++ exercicios/src/fundamentos/TipoString.java
@@ -0,0 +1,30 @@
+package fundamentos;
+
+public class TipoString {
+
+	public static void main(String[] args) {
+
+		System.out.println("Olá pessoal".charAt(2));
+		
+		String s = "Boa tarde";
+		System.out.println(s.concat("!!!"));
+		System.out.println(s + "!!!");
+		System.out.println(s.startsWith("Boa"));
+		System.out.println(s.toLowerCase().startsWith("boa"));
+		System.out.println(s.toUpperCase().endsWith("TARDE"));
+		System.out.println(s.length());
+		System.out.println(s.equalsIgnoreCase("boa tarde"));
+		
+		var nome = "Bruno";
+		var sobrenome = "Alves";
+		var idade = 31;
+		var salario = 12345.98;
+		
+		System.out.println("Nome: " + nome + "\nSobrenome: " + sobrenome +
+				"\nIdade: " + idade + "\nSalario: " + salario + "\n\n");
+		System.out.printf("Nome: %s %s\nIdade: %d anos\nSalário: R$ %.2f", nome, sobrenome, idade, salario);
+		
+		String frase = String.format("Nome: %s %s\nIdade: %d anos\nSalário: R$ %.2f", nome, sobrenome, idade, salario);
+	}
+
+}
