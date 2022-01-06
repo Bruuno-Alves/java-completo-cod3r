52042246f6e02d6aea41d85f8f6b5acd856dfe86
Bruuno-Alves <bruno.a.santos@live.com>
2022-01-05 22:51:41 -0300

    Estruturas de controle

---
+++ exercicios/src/controle/DesafioIf.java
@@ -0,0 +1,16 @@
+package controle;
+
+public class DesafioIf {
+
+	public static void main(String[] args) {
+
+		double nota = 1.3;
+		
+		//Não usar ; ao terminar uma estrutura de controle (há uma exceção)
+		if (nota >= 9) {
+			System.out.println("Parabéns! Você é fera!!");
+			System.out.println("Quadro de honra!");
+		}
+	}
+
+}
