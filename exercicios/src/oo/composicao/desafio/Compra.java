40044687b25fd103a7013517c5454426bb3e29cd
Bruuno-Alves <bruno.a.santos@live.com>
2022-06-13 23:24:11 -0300

    Orientacao a objetos, composicao

---
+++ exercicios/src/oo/composicao/desafio/Compra.java
@@ -0,0 +1,14 @@
+package oo.composicao.desafio;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Compra {
+	
+	List<Item> itens = new LinkedList<>();
+	
+	double obterValorTotal() {
+		return 0;
+	}
+
+}
---
+++ exercicios/src/oo/composicao/desafio/Item.java
@@ -0,0 +1,12 @@
+package oo.composicao.desafio;
+
+public class Item {
+	
+	Produto produto;
+	int quantidade;
+	
+	Item(Produto protudo, int quantidade) {
+		this.produto = produto;
+	}
+
+}
