64fb691f7e6636b76e06003ae2bc44e07fb86265
Bruuno-Alves <bruno.a.santos@live.com>
2022-07-07 23:54:32 -0300

    Campo

---
+++ campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
@@ -0,0 +1,73 @@
+package br.com.cod3r.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.cod3r.cm.excecao.ExplosaoException;
+
+public class Campo {
+	
+	private final int LINHA;
+	private final int COLUNA;
+	
+	private boolean aberto = false;
+	private boolean minado = false;
+	private boolean marcado = false;
+	
+	private List<Campo> vizinhos = new ArrayList<>();
+	
+	Campo(int linha, int coluna) {
+		this.LINHA = linha;
+		this.COLUNA = coluna;
+	}
+	
+	boolean adicionarVizinho(Campo vizinho) {
+		boolean linhaDiferente = this.LINHA != vizinho.LINHA;
+		boolean colunaDiferente = this.COLUNA != vizinho.COLUNA;
+		boolean diagonal = linhaDiferente && colunaDiferente;
+		
+		int deltaLinha = Math.abs(this.LINHA - vizinho.LINHA);
+		int deltaColuna = Math.abs(this.COLUNA - vizinho.COLUNA);
+		int deltaTotal = deltaLinha + deltaColuna;
+		
+		if(deltaTotal == 1 && !diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		} else if(deltaTotal == 2 && diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	void alternarMarcacao() {
+		if(!aberto) {
+			marcado = !marcado;
+		}
+	}
+	
+	boolean abrir() {
+		
+		if(!aberto && !marcado) {
+			aberto = true;
+			
+			if(minado) {
+				throw new ExplosaoException();
+			}
+			
+			if(vizinhancaSegura()) {
+				vizinhos.forEach(v -> v.abrir());
+			}
+			
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	boolean vizinhancaSegura() {
+		return vizinhos.stream().noneMatch(v -> v.minado);
+	}
+
+}
